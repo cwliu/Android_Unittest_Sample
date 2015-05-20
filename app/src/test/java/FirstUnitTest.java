bb718f4dcdaf80b5512069f201ede2ef92325e6b
Cody Liu <monmo.liu@gmail.com>
2015-05-20 20:48:09 +0800

    First commit

---
+++ app/src/test/java/FirstUnitTest.java
@@ -0,0 +1,12 @@
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class FirstUnitTest{
+
+    @Test
+    public void testInteger() {
+        // assertTrue(false);
+        assertEquals(1, 1);
+    }
+}
